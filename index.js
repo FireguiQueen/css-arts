5ccecaebb95666aca3c3849fb8b70280df4e79e9
Guilherme <Fireguiquenbr@gmail.com>
2023-01-08 18:12:35 -0300

    Update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-function CreatCard(nome, desenho, redirecionamento){
-    let PageCard = WhatDraw => window.location.href = `./${WhatDraw}/index.html`;
+function CreatCard(nome, desenho){
     return`
-    <a href="https://youtube.com" target="blank">
-        <div id="card" onclick="PageCard(${redirecionamento})" style="background-image: url(./images/${desenho}.png);">
+    <a href="./${desenho}/Index.html" target="blank">
+        <div id="card" style="background-image: url(./images/${desenho}.png);">
             <ul id="draws-name">
                 <li>${nome}</li>
             </ul>
@@ -13,14 +12,14 @@ function CreatCard(nome, desenho, redirecionamento){
 
 var section = document.getElementById('draws');
 section.innerHTML = 
-CreatCard('Clã Uchiha', 'Uchiha', 'Uchiha') + 
-CreatCard('Cubo', 'Cube', 'Cubo') + 
-CreatCard('Flor', 'Flower', 'Flower') + 
-CreatCard('Gato', 'Cat', 'Cat') + 
-CreatCard('Clã Uchiha', 'Uchiha', 'Uchiha') + 
-CreatCard('Clã Uchiha', 'Uchiha', 'Uchiha') + 
-CreatCard('Clã Uchiha', 'Uchiha', 'Uchiha') + 
-CreatCard('Clã Uchiha', 'Uchiha', 'Uchiha') 
+CreatCard('Clã Uchiha', 'Uchiha') + 
+CreatCard('Cubo', 'Cube') + 
+CreatCard('Flor', 'Flower') + 
+CreatCard('Gato', 'Cat') + 
+CreatCard('Clã Uchiha', 'Uchiha') + 
+CreatCard('Clã Uchiha', 'Uchiha') + 
+CreatCard('Clã Uchiha', 'Uchiha') + 
+CreatCard('Clã Uchiha', 'Uchiha') 
 
 
 
